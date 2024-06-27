023d540484eaabffaea78bbb633fd6d6239bc06f
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-06-27 21:58:57 +0300

    adding a css for the list item when the deadline is passed

--- To-Do-List/Scripts/index.js
+++ To-Do-List/Scripts/index.js
@@ -3,10 +3,12 @@ let taskInput = document.getElementById("taskInput");
 let taskList = document.querySelector(".list");
 let UserNameInput = document.getElementById("usernameInput");
 let TimeInput = document.getElementById("timeInput");
+
 addTaskBtn.addEventListener("click", function () {
   let taskText = taskInput.value.trim();
   let nameText = UserNameInput.value.trim();
   let timeText = TimeInput.value.trim();
+
   if (taskText !== "" && nameText !== "" && timeText !== "") {
     let newTaskItem = document.createElement("li");
     newTaskItem.innerHTML = ` ${taskText} <br> ${nameText} <span>|</span> ${timeText}`;
@@ -14,11 +16,43 @@ addTaskBtn.addEventListener("click", function () {
     taskInput.value = "";
     UserNameInput.value = "";
     TimeInput.value = "";
+
+   
+    checkDeadlinePassed(newTaskItem, timeText);
+    
     newTaskItem.addEventListener("click", function () {
       newTaskItem.style.textDecoration = "line-through";
     });
   }
 });
+
+function checkDeadlinePassed(taskItem, timeText) {
+  
+  let currentDate = new Date();
+  
+  
+  let [hours, minutes] = timeText.split(':');
+  
+  
+  let taskTime = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), hours, minutes);
+  
+ 
+  if (taskTime < currentDate) {
+    taskItem.style.color = "red"; 
+  } else {
+    
+    let interval = setInterval(function () {
+      currentDate = new Date(); 
+      if (taskTime < currentDate) {
+        taskItem.style.color = "red";
+        clearInterval(interval);
+      }
+    }, 60000); 
+  }
+}
+
+
+
 
 function formatDate(date) {
   const day = date.getDate();
